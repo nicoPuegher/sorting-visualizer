28cc8971245f46154a235f78cb4427e09c34aaa6
Nicolas <nicopuegher@gmail.com>
2024-05-16 14:24:25 -0300

    Import Bars component in App.jsx and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Controls from './components/Controls/Controls';
+import Bars from './components/Bars/Bars';
 
 function App() {
 	return (
 		<>
 			<h1>Sorting Visualizer</h1>
+			<Bars />
 			<Controls />
 		</>
 	);
