de574ffaa63fa375354a8bb58cc35eaed3823a40
Nicolas <nicopuegher@gmail.com>
2024-05-18 16:49:52 -0300

    Create a Sorting component to render Bars and Controls

---
+++ src/components/Sorting/Sorting.jsx
@@ -0,0 +1,13 @@
+import Bars from '../Bars/Bars';
+import Controls from '../Controls/Controls';
+
+function Sorting() {
+	return (
+		<>
+			<Bars />
+			<Controls />
+		</>
+	);
+}
+
+export default Sorting;
