61cf528e4a471de5c7d1d82f76bdc2fccfe7f70f
Nicolas <nicopuegher@gmail.com>
2024-05-19 17:10:32 -0300

    Use showBars(boolean) to render content dynamically

--- src/components/Bars/Bars.jsx
+++ src/components/Bars/Bars.jsx
@@ -1,9 +1,9 @@
 import styles from './Bars.module.css';
 
-function Bars() {
-	const message = <h2>Generate an array</h2>;
+function Bars({ showBars }) {
+	const content = showBars ? <h2>Bars</h2> : <h2>Generate an array</h2>;
 
-	return <div className={styles.container}>{message}</div>;
+	return <div className={styles.container}>{content}</div>;
 }
 
 export default Bars;
