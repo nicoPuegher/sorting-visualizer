c4e51d1a70733e1c722cd137167174e348238393
Nicolas <nicopuegher@gmail.com>
2024-05-19 17:07:04 -0300

    Track window.innerWidth(px) and showBars(boolean) with useState in Sorting.jsx

--- src/components/Sorting/Sorting.jsx
+++ src/components/Sorting/Sorting.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import Bars from '../Bars/Bars';
 import Controls from '../Controls/Controls';
 
 function Sorting() {
+	const [showBars, setShowBars] = useState(false);
+	const [width, setWidth] = useState(window.innerWidth);
+
 	return (
 		<>
 			<Bars />
