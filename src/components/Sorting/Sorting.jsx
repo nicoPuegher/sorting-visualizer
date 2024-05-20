17b798c5acac3e6d6e728a6ea10a0b96b2229063
Nicolas <nicopuegher@gmail.com>
2024-05-20 17:21:29 -0300

    Add useEffect to hide bars when the width changes in Sorting.jsx

--- src/components/Sorting/Sorting.jsx
+++ src/components/Sorting/Sorting.jsx
@@ -1,10 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Bars from '../Bars/Bars';
 import Controls from '../Controls/Controls';
 
 function Sorting() {
 	const [showBars, setShowBars] = useState(false);
 	const [width, setWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth !== width) {
+				setShowBars(false);
+				setWidth(window.innerWidth);
+			}
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, [width]);
 
 	const handleShowBars = () => setShowBars(true);
 
