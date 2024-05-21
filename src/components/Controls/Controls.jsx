23347433713da4fa11dcf692f0e1b21e2575a057
Nicolas <nicopuegher@gmail.com>
2024-05-21 12:00:39 -0300

    Add PropTypes in Controls.jsx

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styles from './Controls.module.css';
 
 function Controls({ onDisplay }) {
@@ -9,5 +10,9 @@ function Controls({ onDisplay }) {
 		</div>
 	);
 }
+
+Controls.propTypes = {
+	onDisplay: PropTypes.func.isRequired,
+};
 
 export default Controls;
