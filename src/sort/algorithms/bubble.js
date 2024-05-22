35c5e9878cdd66208f7e69bbe7b0f7270ce78f15
Nicolas <nicopuegher@gmail.com>
2024-05-22 13:59:56 -0300

    Write bubble sort algorithm

--- src/sort/algorithms/bubble.js
+++ src/sort/algorithms/bubble.js
@@ -1,3 +1,20 @@
-const bubble = (sortArray) => {};
+const bubble = (array) => {
+	const copy = array.slice();
+	let len = copy.length;
+	let swapped = true;
+
+	do {
+		swapped = false;
+		for (let i = 0; i < len - 1; i++) {
+			if (copy[i] > copy[i + 1]) {
+				[copy[i], copy[i + 1]] = [copy[i + 1], copy[i]];
+				swapped = true;
+			}
+		}
+		len--;
+	} while (swapped);
+
+	return copy;
+};
 
 export default bubble;
