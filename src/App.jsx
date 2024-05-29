5346a99c117cd6ffc5a114dfdef96661c10c2d6e
Nicolas <nicopuegher@gmail.com>
2024-05-29 15:35:01 -0300

    Add a header element to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Sorting from './components/Sorting/Sorting';
 function App() {
 	return (
 		<>
-			<h1>Sorting Visualizer</h1>
+			<header>
+				<h1>Sorting Visualizer</h1>
+			</header>
 			<Sorting />
 		</>
 	);
