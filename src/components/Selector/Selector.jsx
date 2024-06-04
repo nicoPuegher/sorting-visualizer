d8c0f6ea66972976c35cc4eacaca6cd39c70fa2d
Nicolas <nicopuegher@gmail.com>
2024-06-04 13:21:34 -0300

    Return a form with a select from the Selector component

--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -1,5 +1,23 @@
 import styles from './Selector.module.css';
 
-function Selector() {}
+function Selector() {
+	return (
+		<form className={styles.form}>
+			<label id="selector">Sorting</label>
+
+			<select aria-labelledby="selector">
+				<option value="bubble">Bubble</option>
+				<option value="selection">Selection</option>
+				<option value="insertion">Insertion</option>
+				<option value="quick">Quick</option>
+				<option value="merge">Merge</option>
+				<option value="heap">Heap</option>
+				<option value="counting">Counting</option>
+			</select>
+
+			<button type="submit">Sort</button>
+		</form>
+	);
+}
 
 export default Selector;
