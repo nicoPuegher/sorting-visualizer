c6b3f27f155c4403949a942d3e53cabf0e3c8698
Nicolas <nicopuegher@gmail.com>
2024-06-10 12:43:00 -0300

    Change bubble sort algorithm to track every movement

--- src/sort/algorithms/bubble.js
+++ src/sort/algorithms/bubble.js
@@ -1,20 +1,20 @@
-const bubble = (array) => {
-	const copy = array.slice();
-	let len = copy.length;
+const bubble = (state, setState) => {
+	const array = [...state.arr];
+	let jLen = array.length;
 	let swapped;
+	const animations = [];
 
-	do {
-		swapped = false;
-		for (let i = 0; i < len - 1; i++) {
-			if (copy[i] > copy[i + 1]) {
-				[copy[i], copy[i + 1]] = [copy[i + 1], copy[i]];
-				swapped = true;
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < jLen - 1; j++) {
+			animations.push([j, j + 1, false, false]);
+			if (array[j].height > array[j + 1].height) {
+				[array[j], array[j + 1]] = [array[j + 1], array[j]];
+				animations[animations.length - 1][2] = true;
 			}
 		}
-		len--;
-	} while (swapped);
-
-	return copy;
+		animations[animations.length - 1][3] = true;
+		jLen--;
+	}
 };
 
 export default bubble;
