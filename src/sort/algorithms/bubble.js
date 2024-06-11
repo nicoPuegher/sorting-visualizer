626911e04b45914318852744d30f605f44b23c71
Nicolas <nicopuegher@gmail.com>
2024-06-11 17:49:23 -0300

    Refactor bubble sort algorithm

--- src/sort/algorithms/bubble.js
+++ src/sort/algorithms/bubble.js
@@ -1,48 +1,71 @@
-const bubble = (state, setState) => {
-	const array = [...state.arr];
-	let jLen = array.length;
-	const animations = [];
+const SPEED = 10;
 
-	for (let i = 0; i < array.length; i++) {
-		for (let j = 0; j < jLen - 1; j++) {
-			animations.push([j, j + 1, false, false]);
+const generateAnimations = (array, animations) => {
+	array.forEach((_, i) => {
+		// Sort an item and remove it from the iteration
+		for (let j = 0; j < array.length - i - 1; j++) {
+			// Current bars for comparison [current, next, isCompared, isSorted]
+			const comparison = [j, j + 1, false, false];
+			animations.push(comparison);
+
+			// If next bar is greater, swap it
 			if (array[j].height > array[j + 1].height) {
 				[array[j], array[j + 1]] = [array[j + 1], array[j]];
-				animations[animations.length - 1][2] = true;
+				comparison[2] = true;
 			}
 		}
-		animations[animations.length - 1][3] = true;
-		jLen--;
-	}
 
-	for (let i = 0; i < animations.length; i++) {
+		// Sorted item
+		animations[animations.length - 1][3] = true;
+	});
+};
+
+const displayAnimations = (setState, animations) => {
+	animations.forEach((animation, i) => {
+		// Adjust SPEED variable to control the sorting speed
 		setTimeout(() => {
 			setState((s) => {
-				const [j, k, swapped, sorted] = animations[i];
-				const newArr = [...s.arr];
-				newArr[j].isCompared = true;
-				newArr[k].isCompared = true;
+				const [j, k, swapped, sorted] = animation;
+				const copyArr = [...s.arr];
+
+				// Current comparison
+				copyArr[j].isCompared = true;
+				copyArr[k].isCompared = true;
+
+				// Turn previous item back to normal
 				if (j > 0) {
-					newArr[j - 1].isCompared = false;
+					copyArr[j - 1].isCompared = false;
 				}
 
+				// Perform swap animation
 				if (swapped) {
-					[newArr[j], newArr[k]] = [newArr[k], newArr[j]];
+					[copyArr[j], copyArr[k]] = [copyArr[k], copyArr[j]];
 				}
 
-				if (sorted == true) {
-					newArr[j].isCompared = false;
-					newArr[k].isSorted = true;
-					newArr[k].isCompared = false;
+				// Item is sorted
+				if (sorted) {
+					copyArr[j].isCompared = false;
+					copyArr[k].isSorted = true;
+					copyArr[k].isCompared = false;
 				}
 
+				// Set last item (first) as sorted
 				if (i == animations.length - 1) {
-					newArr[j].isSorted = true;
+					copyArr[j].isSorted = true;
 				}
-				return { ...s, arr: newArr };
+
+				return { ...s, arr: copyArr };
 			});
-		}, i * 10);
-	}
+		}, i * SPEED);
+	});
+};
+
+const bubble = (state, setState) => {
+	const array = [...state.arr];
+	const animations = [];
+
+	generateAnimations(array, animations);
+	displayAnimations(setState, animations);
 };
 
 export default bubble;
