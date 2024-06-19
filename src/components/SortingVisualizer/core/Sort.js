f1cbdf9f45d6d41f6225ccd8f87bd17c5fb680fd
Nicolas <nicopuegher@gmail.com>
2024-06-19 14:03:25 -0300

    Update selection sort to work with the new structure

--- src/components/SortingVisualizer/core/Sort.js
+++ src/components/SortingVisualizer/core/Sort.js
@@ -1,8 +1,13 @@
 import bubbleSort from '../algorithms/bubble.js';
+import selectionSort from '../algorithms/selection.js';
 
 class Sort {
 	static bubble(barChart, setBarChart) {
 		return bubbleSort(barChart, setBarChart);
+	}
+
+	static selection(barChart, setBarChart) {
+		return selectionSort(barChart, setBarChart);
 	}
 }
 
