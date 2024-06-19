08d60a2fcddb6406acacd269813c0c2058c4e3c6
Nicolas <nicopuegher@gmail.com>
2024-06-19 14:13:32 -0300

    Remove unused options in Selector component

--- src/components/Selector/Selector.jsx
+++ src/components/Selector/Selector.jsx
@@ -25,9 +25,6 @@ function Selector({ onSubmit, onSelect }) {
 				<option value="selection">Selection</option>
 				<option value="insertion">Insertion</option>
 				<option value="quick">Quick</option>
-				<option value="merge">Merge</option>
-				<option value="heap">Heap</option>
-				<option value="counting">Counting</option>
 			</select>
 
 			<button type="submit">Sort</button>
