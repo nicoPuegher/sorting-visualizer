604ecf0961ba44655f1583a48dad102e5952f9d2
Nicolas <nicopuegher@gmail.com>
2024-06-19 14:06:10 -0300

    Update insertion sort to work with the new structure

---
+++ src/components/SortingVisualizer/algorithms/insertion.js
@@ -0,0 +1,37 @@
+import Animation from '../core/Animation.js';
+import displayAnimations from '../helpers/displayAnimations.js';
+
+const generateAnimations = (array) => {
+	const copy = structuredClone(array);
+	const len = copy.length;
+	const animations = [];
+
+	for (let i = 1; i < len; i++) {
+		let temporal = copy[i];
+		let j = i - 1;
+
+		// Push a comparison animation
+		Animation.comparison(animations, j, i);
+
+		// Swap items
+		while (j >= 0 && copy[j].height > temporal.height) {
+			copy[j + 1] = copy[j];
+			copy[j] = temporal;
+
+			// Push a comparison and swap animation
+			Animation.comparison(animations, j + 1, j);
+			Animation.swap(animations, j + 1, j);
+
+			j--;
+		}
+	}
+
+	return animations;
+};
+
+const insertion = (barChart, setBarChart) => {
+	const animations = generateAnimations(barChart.array);
+	displayAnimations(setBarChart, animations);
+};
+
+export default insertion;
--- src/components/SortingVisualizer/core/Sort.js
+++ src/components/SortingVisualizer/core/Sort.js
@@ -1,5 +1,6 @@
 import bubbleSort from '../algorithms/bubble.js';
 import selectionSort from '../algorithms/selection.js';
+import insertionSort from '../algorithms/insertion.js';
 
 class Sort {
 	static bubble(barChart, setBarChart) {
@@ -8,6 +9,10 @@ class Sort {
 
 	static selection(barChart, setBarChart) {
 		return selectionSort(barChart, setBarChart);
+	}
+
+	static insertion(barChart, setBarChart) {
+		return insertionSort(barChart, setBarChart);
 	}
 }
 
