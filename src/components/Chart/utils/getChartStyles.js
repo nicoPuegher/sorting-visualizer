c369e2e9a473c773101d71c2a78376aaac0efdbe
Nicolas <nicopuegher@gmail.com>
2024-06-23 12:45:31 -0300

    Add comments to getChartStyles utility fn

--- src/components/Chart/utils/getChartStyles.js
+++ src/components/Chart/utils/getChartStyles.js
@@ -1,3 +1,4 @@
+// Apply .chart class when displayChart state is true and remove it when is false
 const getChartStyles = (displayChart, styles) => {
 	return displayChart
 		? `${styles.container} ${styles.chart}`
