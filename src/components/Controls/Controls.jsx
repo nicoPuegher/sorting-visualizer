90db087166fc8326eaf55ffb9be0b783faf05527
Nicolas <nicopuegher@gmail.com>
2024-06-24 12:54:17 -0300

    Add comments to Controls component

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import styles from './Controls.module.css';
 
+// Allow user to generate a new chart or start a sorting animation
 function Controls({ isAnimationActive, onDisplay, onSubmit }) {
 	return (
 		<div className={styles.container}>
